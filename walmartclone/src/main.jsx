831bade054031b3d2997bfb944f5f8ae0ff42457
payal bhattamisra <payalbhattamisra@gmail.com>
2024-08-01 18:18:32 +0530

    changes

--- walmartclone/src/main.jsx
+++ walmartclone/src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
- 
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./style/index.css";
+import { AuthProvider } from "./AuthContext/AuthProvider";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+ <AuthProvider>
+  <ChakraProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ChakraProvider>
+ </AuthProvider>
+);
